195e833ebf506e109f6095c995b0d290f4123724
Markus Rei. <markus.reinumae@tktk.ee>
2024-05-01 16:15:50 +0300

    Week4 Loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int i = 0;
+for(i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
